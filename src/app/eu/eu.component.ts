06ea915a3f30ae44f0c4c168cb247416aed9288d
Jesus Pasillas'git config --global user.name Jesus <jpasillas11@gmail.com>
2020-11-10 18:39:36 -0300

    Adding photo

--- src/app/eu/eu.component.ts
+++ src/app/eu/eu.component.ts
@@ -8,8 +8,9 @@ import { Component, OnInit } from '@angular/core';
 export class EuComponent implements OnInit {
 
   nome = 'Jesus Pasillas';
-  info = 'estoy aqui en aulas';
-  img = '';
+  info = 'Eu gosto de esportes, tanto assistir como jogar, gosto da natureza e de bichos; ate morar no Brasil fiquei sabendo que minha comida favorita são os tacos.';
+  img = './assets/img/_MG_6813_1.JPG';
+  title = 'app'
 
 
   constructor() { }
